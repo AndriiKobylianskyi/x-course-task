2e4db0c95a8595fee1cb24e2b06b8cd9fe5a5d54
Andrew Koba <kobasap2283228@gmail.com>
2023-03-07 01:28:44 +0200

    Change width

--- src/components/signIn/SignIn.js
+++ src/components/signIn/SignIn.js
@@ -49,9 +49,9 @@ const SignIn = () => {
         <div className="content">
             <div className="flex-container">
                 <div>
-                    <img src={signImg} alt="books" width="795" height="700" className={styles.img} />
+                    <img src={signImg} alt="books" width="745" height="650" className={styles.img} />
                 </div>
-                <div className={styles.pr16}>
+                <div >
                     <img src={avatarBig} alt="avatar" className={styles.avatar} />
                     <p className={styles.username}>username</p>
                     <input type="text" className={styles.input} placeholder="Type username"
